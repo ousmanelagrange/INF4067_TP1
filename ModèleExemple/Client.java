f34f8947dd013116bc5ee74c61179a4bc3178603
ousmanemfochive4@gmail.com <ousmanou.njikoufon@facsciences-uy1.cm>
2023-10-05 23:18:48 +0100

    Implemntation de l'Exemple

---
+++ ModèleExemple/Client.java
@@ -0,0 +1,14 @@
+public class client {
+
+    public static void main(String [] args) {
+        ProduitFactory produitFactory = new ProduitFactory();
+
+        ProduitA produitA = null;
+        produitA = produitFactory.getProduitA(produitFactory.TYPE_PRODUIT_A1);
+        produitA.methodA();
+
+        produitA = produitFactory.getProduitA(produitFactory.TYPE_PRODUIT_A2);
+        produitA.methodA();
+        
+    }
+}
