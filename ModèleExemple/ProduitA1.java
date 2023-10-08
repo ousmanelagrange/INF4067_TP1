068965dd0fb61fc494d2378ea224a550aac61138
ousmanemfochive4@gmail.com <ousmanou.njikoufon@facsciences-uy1.cm>
2023-10-08 17:36:58 +0100

    renommage des repertoires

--- ModèleExemple/ProduitA1.java
+++ ModèleExemple/ProduitA1.java
@@ -2,6 +2,6 @@ public class ProduitA1 extends ProduitA {
 
     public void methodA() {
         System.out.println("Je suis un produit de type A1");
-        System.out.println("ProduitA1.methodeA1()");
+        System.out.println("ProduitA1.methodeA()");
     }
 }
