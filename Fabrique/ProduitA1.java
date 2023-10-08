a80a55be1befeacb14e7c42c6fe81191d29c8deb
ousmanemfochive4@gmail.com <ousmanou.njikoufon@facsciences-uy1.cm>
2023-10-08 18:11:49 +0100

    creation du répertoire AbstractFactory

--- Fabrique/ProduitA1.java
+++
@@ -1,7 +0,0 @@
-public class ProduitA1 extends ProduitA {
-
-    public void methodA() {
-        System.out.println("Je suis un produit de type A1");
-        System.out.println("ProduitA1.methodeA()");
-    }
-}   
