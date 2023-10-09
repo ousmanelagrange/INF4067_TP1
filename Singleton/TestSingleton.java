81102784bfa5cfc01eec8403c41494059be3fafc
ousmanemfochive4@gmail.com <ousmanou.njikoufon@facsciences-uy1.cm>
2023-10-09 03:33:36 +0100

    new file:   Singleton/TestSingleton.java

---
+++ Singleton/TestSingleton.java
@@ -0,0 +1,14 @@
+package Singleton;
+public class TestSingleton {
+    public static void main(String [] args) {
+        int somme = Singleton.getInstance().somme(2,5);
+
+        System.out.printf("La somme est : %d", somme);
+        Singleton s1 = Singleton.getInstance(8, 3);
+        s1.afficher();
+
+        Singleton s2 = Singleton.getInstance(5, 9);
+        s2.afficher();
+        s1.afficher();
+    }
+}
